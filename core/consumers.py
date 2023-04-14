9280b20a9f8ab1fe196cbe9fef1be6a314f10c77
Рустам Астафеев <astafeev0308@gmail.com>
2023-04-14 12:40:33 +0500

    smol update

--- core/consumers.py
+++
@@ -1,8 +0,0 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
-
-
-class AsyncHealthCheckConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        await self.accept()
-        await self.send('ok')
-        await self.close()
