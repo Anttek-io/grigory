4f90d5108185fac7e7d722da69c5c2573dfa18d9
Рустам Астафеев <astafeev0308@gmail.com>
2023-05-04 17:38:42 +0500

    More security

--- app/serializers/messages.py
+++ app/serializers/messages.py
@@ -1,12 +1,22 @@
 from rest_framework import serializers
 
-from app.models import Message
+from app.models import Message, Chat
 from app.serializers.chats import ChatSerializer
 from app.serializers.users import UserSerializer
 
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
+
+    def get_chat_queryset(self):
+        if self.context.get('request'):
+            return self.context['request'].user.chats.all()
+        return Chat.objects.none()
+
+    def get_extra_kwargs(self):
+        kwargs = super().get_extra_kwargs()
+        kwargs["chat"] = {"queryset": self.get_chat_queryset()}
+        return kwargs
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
