6e9453320ae0ddc88a2eacd732cbf5125b455ece
Рустам Астафеев <astafeev0308@gmail.com>
2023-05-04 17:34:02 +0500

    Updates

--- app/serializers/messages.py
+++ app/serializers/messages.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
 
 from app.models import Message
+from app.serializers.chats import ChatSerializer
 from app.serializers.users import UserSerializer
 
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['chat'] = ChatSerializer(instance.chat).data
+        return data
 
     def create(self, validated_data):
         validated_data['sender'] = self.context['request'].user
